cf2270d45be3a9cf75966e86acfb1648d27ced45
Elisebassett <elise@iileaders.com>
2018-04-10 19:36:27 -0600

    working typewriter header and minor style changes

--- app/assets/javascripts/typing_effect.js
+++ app/assets/javascripts/typing_effect.js
@@ -1,45 +1,67 @@
+$(document).on('turbolinks:load', function(){
+  $('.sub').hide();
+});
+
+
 $(document).ready(function(){
+
   var x = null;
   var content = '';
   var headEl = $('#head');
   var subEl = $('#sub');
-  var head = '';
-  var headers = ['Oh, hey there!','Welcome to my site!', "My name is Elise Bassett.", "I'm a Full Stack Web Developer."]; 
-  var headLength = 0;
+  var string = '';
+  var subString = '';
+  // var strings = ['test 1', 'testasd 2', 'tedfsaastkjhkkjlkjlk 3'];
+  var strings = ['Oh, hey there!','Welcome to my site!', "My name is Elise Bassett", "A Full Stack Web Developer"]; 
+  var stringLength = 0;
   var lineCount = 0;
   
   function typeHeader() {
     x = 0;
-    head = headers[lineCount];
-    headLength = head.length;
+    string = strings[lineCount];
+    stringLength = string.length;
     
-    if(lineCount < headers.length) {
+    if(lineCount < strings.length) {
       type();
       lineCount = lineCount + 1
     }
-    console.log(lineCount, headers.length);
+    console.log(lineCount, strings.length);
   }//typeHeader
   
   function type() {
-    headEl.html(head.substr(0, x++));
-    if(x < head.length + 1) {
-      setTimeout(type, 100); 
-    } else if(lineCount < headers.length - 1) {
-      erase();
-    } else if(lineCount = headers.length) {
-
-    }
+    headEl.html(string.substr(0, x++));
+    if(x < string.length + 1) {
+      setTimeout(type, 120); 
+    } else if(lineCount < strings.length - 1) {
+      setTimeout(erase, 700);
+    } else if(lineCount === strings.length - 1) {
+      console.log('sub');
+      $('#cursor_sub').addClass('cursor');
+      $('#cursor_head').removeClass('cursor').remove();
+      $('.sub').show();
+      x = 0;
+      subType();
+    }//else ifs
   }//typeEffect
+
+  function subType(){
+    subString = strings[lineCount];
+    subEl.html(subString.substr(0, x++));    
+    if(x < subString.length + 1) {
+      setTimeout(subType, 120); 
+    }//ifelse
+  }//subtype 
   
   function erase() {
     content = headEl.html();
     console.log(content);
-    headEl.html(content.substring(0, headLength--));
-    if(headLength >= 0) {
+
+    headEl.html(content.substring(0, stringLength--));
+    if(stringLength >= 0) {
       setTimeout(erase, 100);
-    } else if(lineCount < headers.length) {
+    } else if(lineCount < strings.length) {
       typeHeader();
-    }
+    }//ifelse
   }
   
   function cursorAnimation(){
@@ -48,8 +70,14 @@ $(document).ready(function(){
     cursor.fadeIn('fast');
   }//cursor animation
   
+
+//// Document Ready
+
+
   
   setInterval(cursorAnimation, 600);
   typeHeader(); 
+
+
 
 });
